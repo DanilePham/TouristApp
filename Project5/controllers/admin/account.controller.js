21aa35845100d7913c2fc358b2239a6ec39d42fd
DanilePham <s3966848@rmit.edu.vn>
2025-11-24 08:12:23 +1100

    intergrating bcrypr

--- Project5/controllers/admin/account.controller.js
+++ Project5/controllers/admin/account.controller.js
@@ -1,4 +1,5 @@
 const { AccountAdmin } = require('../../models/account-admin.model');
+const bcrypt=require("bcryptjs");
  
  module.exports.getAccountPage = (req, res) => {
   res.render('admin/page/login', { pagetitle: "Log in" })
@@ -22,6 +23,9 @@ const { AccountAdmin } = require('../../models/account-admin.model');
   }
 
   req.body.status = "active"; //initialize status field
+
+  //Hash password before saving to database
+  req.body.password= await bcrypt.hash(req.body.password,10);
 
   const newAccount = new AccountAdmin(req.body);
   await newAccount.save();
