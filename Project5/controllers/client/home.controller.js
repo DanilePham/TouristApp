8e52d282ed6d388eb2d36c14c5c4786eeb541c09
DanilePham <s3966848@rmit.edu.vn>
2026-01-05 13:04:27 +0700

    Express the data into the section 2

--- Project5/controllers/client/home.controller.js
+++ Project5/controllers/client/home.controller.js
@@ -1,3 +1,26 @@
- module.exports.getHomePage = (req, res) => {
-  res.render('client/pages/home.pug', { pagetitle: "123" })
+const Tour = require('../../models/tour.model.js');
+const moment = require('moment');
+
+
+module.exports.getHomePage = async (req, res) => {
+  //section 2
+  const tourListSection2 = await Tour.find({
+    deleted: false,
+    status: "active",
+  }).sort({
+    position: "desc"
+  }).limit(6);
+
+  for (const item of tourListSection2) {
+    if(item.departureDate){
+      item.departureDateFormat = moment(item.departureDate).format("DD/MM/YYYY");
+    }
+  }
+
+  console.log(tourListSection2);
+  //end Section2 
+
+
+
+  res.render('client/pages/home.pug', { pagetitle: "123", tourListSection2: tourListSection2 });
 }
