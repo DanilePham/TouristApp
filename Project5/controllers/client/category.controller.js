ec7deca929faabab9dd9b5615bd32213b151cb56
DanilePham <s3966848@rmit.edu.vn>
2026-01-07 14:04:17 +0700

    express list of tours for client

--- Project5/controllers/client/category.controller.js
+++ Project5/controllers/client/category.controller.js
@@ -1,4 +1,7 @@
- const { CategoryModel } = require('../../models/categories.model.js');
+const { CategoryModel } = require('../../models/categories.model.js');
+const Tour = require('../../models/tour.model.js');
+const moment = require('moment');
+const { getCategorySubId } = require('../../helpers/category.helper.js');
 
 module.exports.list = async (req, res) => {
     const slug = req.params.slug;
@@ -6,31 +9,57 @@ module.exports.list = async (req, res) => {
     console.log("Category Slug:", slug);
     // You can use the slug to fetch category-specific data if needed
 
-    const categoryDetail= await CategoryModel.findOne(
+    const categoryDetail = await CategoryModel.findOne(
         {
-            slug:slug,
-            deleted:false,
-            status:"active"
+            slug: slug,
+            deleted: false,
+            status: "active"
         }
     )
     console.log("Category Detail:", categoryDetail);
 
-    if(!categoryDetail){  
+    if (!categoryDetail) {
         res.redirect("/");
         return;
     }
+
+    //List of tours
+    const categorySubId = await getCategorySubId(categoryDetail._id);
+
+
+    const tourList = await Tour.find({
+        category: {
+            $in: [
+                categoryDetail._id,
+                ...categorySubId
+            ]
+        },
+        deleted: false,
+        status: "active"
+    })
+        .sort({
+            position: "desc"
+        }).limit(24);
+
+    for (const item of tourList) {
+        if (item.departureDate) {
+            item.departureDateFormat = moment(item.departureDate).format("DD/MM/YYYY");
+        }
+    }
+    //end list of tours
 
     console.log("Category Detail:", categoryDetail);
 
     console.log("Category Slug:", slug);
 
 
-    res.render("client/pages/tour-list", { 
+    res.render("client/pages/tour-list", {
         pageTitle: "Category Page",
-        categoryDetail: categoryDetail
-        
+        categoryDetail: categoryDetail,
+        tourList: tourList
+
     })
-} 
+}
 
 // module.exports.list = async (req, res) => {
 //     console.log("URL:", req.originalUrl);
